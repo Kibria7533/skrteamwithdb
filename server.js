a0eb06048735acc802af7bb627d7fd48cfe57d47
Cse_academia <62127870+CSEacademia@users.noreply.github.com>
2020-10-09 06:50:06 +0600

    first lll

--- server.js
+++ server.js
@@ -27,43 +27,7 @@ connection.once("open", () => {
 app.use(require("./routes/users"));
 
 if (process.env.NODE_ENV === "production") {
-
-    app.use(express.static("client/build"));
-    app.get('/webhook', (req, res) => {
-
-      // Your verify token. Should be a random string.
-      let VERIFY_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN
-        
-      // Parse the query params
-      let mode = req.query['hub.mode'];
-      let token = req.query['hub.verify_token'];
-      let challenge = req.query['hub.challenge'];
-        
-      // Checks if a token and mode is in the query string of the request
-      if (mode && token) {
-      
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-          
-          // Responds with the challenge token from the request
-          console.log('WEBHOOK_VERIFIED');
-          res.status(200).send(challenge);
-        
-        } else {
-          // Responds with '403 Forbidden' if verify tokens do not match
-          res.sendStatus(403);      
-        }
-      }
-    });
-   app.get('/*', function(req, res) {   
-    res.sendFile(path.join(__dirname, 'client','public','index.html'), function(err) {
-      if (err) {
-        console.log(err);
-        res.status(500).send(err)
-      }
-    })
-  })
-
+      app.use(express.static("client/build"));
   }
 
 
@@ -91,7 +55,32 @@ if (process.env.NODE_ENV === "production") {
     }
   
   });
- 
+  app.get('/webhook', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN
+      
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+  });
 app.listen(process.env.PORT||8000,()=>{
     console.log('server started');
 })
